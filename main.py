f664b4520dced765322fb1be965b221a0ddf24f8
CJ445 <cyrilxjacob445@gmail.com>
2024-07-15 21:40:00 +0530

    Yellow detected, bbox working

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import cv2
-
+from PIL import Image
 from util import get_limits
 
 yellow = [0, 255, 255] # yellow in BGR colorspace
@@ -21,7 +21,16 @@ while True:
 
     mask = cv2.inRange(hsvImage, lowerLimit, upperLimit) # defining interval
 
-    cv2.imshow('frame', mask)
+    mask_ = Image.fromarray(mask) # Converting image in the form of NumPy array to actual Image in Pillow
+
+    bbox = mask_.getbbox() # Draws an imaginary box around the detected object and returns the 4 coordinates
+
+    if bbox is not None:
+        x1, y1, x2, y2 = bbox # stores coordinates
+
+        frame = cv2.rectangle(frame, (x1, y1, x2, y2), (0, 255, 0), 5) # frame, coordinates, color, thickness of bbox
+
+    cv2.imshow('frame',frame) # if mask is passed instead of frame then the whole screen would be blank (dark)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
