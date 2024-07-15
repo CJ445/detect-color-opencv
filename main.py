6de730abff0144a317089f934f7596ae862766c9
CJ445 <cyrilxjacob445@gmail.com>
2024-07-15 18:02:55 +0530

    checking ret - reading frame or not

--- main.py
+++ main.py
@@ -5,6 +5,10 @@ cap = cv2.VideoCapture(0)
 while True:
     ret, frame = cap.read()
 
+    if not ret:
+        print("Error reading frame. Exiting.")
+        break
+
     cv2.imshow('frame',frame)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
